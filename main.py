4537d4b707a6350b052553e5707eea4959b4a811
Chris Lyons <52037738+mephmanx@users.noreply.github.com>
2023-08-04 11:38:06 -0400

    adding oauth
    Signed-off-by: Chris Lyons <52037738+mephmanx@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,10 +1,40 @@
-from flask import Flask, request
+from flask import Flask, redirect, request, session, url_for
 from google.cloud import bigquery
 from google.cloud.bigquery import Table
 from authlib.integrations.flask_client import OAuth
 
 app = Flask(__name__)
+
+app.secret_key = '!secret'
+app.config.from_object('config')
+
+CONF_URL = 'https://accounts.google.com/.well-known/openid-configuration'
 oauth = OAuth(app)
+oauth.register(
+    name='google',
+    server_metadata_url=CONF_URL,
+    client_kwargs={
+        'scope': 'openid email profile'
+    }
+)
+
+@app.route('/login')
+def login():
+    redirect_uri = url_for('auth', _external=True)
+    return oauth.google.authorize_redirect(redirect_uri)
+
+
+@app.route('/auth')
+def auth():
+    token = oauth.google.authorize_access_token()
+    session['user'] = token['userinfo']
+    return redirect('/')
+
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect('/')
 
 @app.post("/")
 def getImage():
